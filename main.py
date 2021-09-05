c9ec85c54b8ffa0bfcf0a80329f99c4c54678ed6
naishkachiu <89881557+naishkachiu@users.noreply.github.com>
2021-09-05 04:44:19 +0000

    Base 10 complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 #Loop the program so that we ask for next number after finishing the last question
 #Create a library for mapping characters of all bases (up to 36)
-digit_library = [0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+digit_library = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 while True:
   #Ask for an input number --> input_num, and reverse it
   input_num = str(input('Please enter number:'))[::-1]
@@ -11,7 +11,7 @@ while True:
   output_base = int(input('What base do you wish to output? (2-36)'))
   #Prepare a variable for the final result
   output_num = 0
-  input_library = digit_library[0:input_base-1]
+  input_library = digit_library[0:input_base]
 
   #loop through each character from input_num
   for char in range(len(input_num)):
@@ -26,5 +26,8 @@ while True:
       # Print out the debug information
       print(char, input_num[char], this_place_value, 
       output_num)
+  #Convert Decimal to Output Base
+  
+
   #Print out the final output_num value
-  print('The final result is: ', output_num)
+  print('In base 10 this is: ', output_num)

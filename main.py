7cc016b9991b47d7cdcb2639f2c78ad724167a72
naishkachiu <89881557+naishkachiu@users.noreply.github.com>
2021-09-05 04:34:04 +0000

    Converted to base 10

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ digit_library = [0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V
 while True:
   #Ask for an input number --> input_num, and reverse it
   input_num = str(input('Please enter number:'))[::-1]
+  #Ask for base of input
+  input_base = int(input('What base is the input number in? (2-36)'))
+  #Ask for base of output
+  output_base = int(input('What base do you wish to output? (2-36)'))
   #Prepare a variable for the final result
   output_num = 0
+  input_library = digit_library[0:input_base-1]
 
   #loop through each character from input_num
   for char in range(len(input_num)):
-    #if the character is valid
-    if int(input_num[char]) <= 1:
+    #check if digit is valid
+    if input_num[char] in input_library:
       #Multiply the value by the base to the power of the place number --> this_place_value
-      this_place_value = int(input_num[char]) * (2**char)
+      this_place_value = input_library.index(input_num[char]) * (input_base**char)
 
       #Add this_place_value to the current output_num
       output_num += this_place_value

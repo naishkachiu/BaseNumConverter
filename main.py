954a02682482e2b188f2470e436a79a7b4a94872
naishkachiu <89881557+naishkachiu@users.noreply.github.com>
2021-09-05 03:38:14 +0000

    Rewritten as simple bit by bit converter

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
+# This is a basic binary to decimal converter. It takes the assumption that we are using 8 bit binary numbers.
 
-#Loop the program so that we ask for next number after finishing the last question
-while True:
-  #Ask for an input number --> input_num
-  input_num = str(input('Please enter number:'))
-  #Prepare a variable for the final result
-  output_num = 0
+#Prepare a variable for the final result
+output_num = 0
 
-  #loop through each character from input_num
-  for char in range(len(input_num)):
-    #if the character is valid
-    if int(input_num[char]) <= 1:
-      #Multiply the value by the base to the power of the place number --> this_place_value
-      this_place_value = int(input_num[char]) * (2**(len(input_num)-char-1))
+#Ask for first input number --> input_num
+input_num = input('Please enter 1st bit:')
+output_num += int(input_num) * 1
 
-      #Add this_place_value to the current output_num
-      output_num += this_place_value
-      # The following are for showing process and debugging
-      # Print out the debug information
-      print(char, input_num[char], this_place_value, 
-      output_num)
-  #Print out the final output_num value
-  print('The final result is: ', output_num)
+#Ask for second input number --> input_num
+input_num = input('Please enter 2nd bit:')
+output_num += int(input_num) * 2
+
+#Ask for third input number --> input_num
+input_num = input('Please enter 3rd bit:')
+output_num += int(input_num) * 4
+
+#Ask for fourth input number --> input_num
+input_num = input('Please enter 4th bit:')
+output_num += int(input_num) * 8
+
+#Ask for fifth input number --> input_num
+input_num = input('Please enter 5th bit:')
+output_num += int(input_num) * 16
+
+#Ask for sixth input number --> input_num
+input_num = input('Please enter 6th bit:')
+output_num += int(input_num) * 32
+
+#Ask for first input number --> input_num
+input_num = input('Please enter 7th bit:')
+output_num += int(input_num) * 64
+
+#Ask for first input number --> input_num
+input_num = input('Please enter 8th bit:')
+output_num += int(input_num) * 128
+
+print('The final result is: ', output_num)
